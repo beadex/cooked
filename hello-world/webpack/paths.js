47e70702b8b70e3cbd6e07a622d74b292679ecae
Phong Tran <tranphongbb@icloud.com>
2020-08-20 18:18:48 +0700

    remove unwanted dir

--- hello-world/webpack/paths.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    '@': path.resolve(__dirname, '../src'),
-    '@styles': path.resolve(__dirname, '../src/styles'),
-    '@components': path.resolve(__dirname, '../src/components'),
-    '@constants': path.resolve(__dirname, '../src/constants'),
-    '@utils': path.resolve(__dirname, '../src/utils')
-}
